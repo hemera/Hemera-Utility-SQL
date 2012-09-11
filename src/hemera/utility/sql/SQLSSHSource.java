e6944e340e1394b46900f5bbfd75d0d5728b20be
Yi Wang <ywang@looply.me>
2012-09-11 18:53:12 -0400

    Added ssh port configuration.

--- src/hemera/utility/sql/SQLSSHSource.java
+++ src/hemera/utility/sql/SQLSSHSource.java
@@ -27,33 +27,35 @@ public class SQLSSHSource extends SQLSource {
 	 * identify the attached data source.
 	 * @param host The <code>String</code> address of
 	 * the host.
-	 * @param port The <code>int</code> port of the
-	 * host to connect to.
+	 * @param sshPort The <code>int</code> port used
+	 * to establish the SSH connection.
+	 * @param sshUsername The <code>String</code> SSH
+	 * user name used login to the SSH host.
+	 * @param sshKey The SSH key <code>File</code>.
+	 * @param dbPort The <code>int</code> port of the
+	 * host database to connect to.
 	 * @param dbName The <code>String</code> name of
 	 * the database.
 	 * @param dbUsername The <code>String</code> user
 	 * name used to login to the database.
 	 * @param dbPassword The <code>String</code> password
 	 * used to login to the database.
-	 * @param sshKey The SSH key <code>File</code>.
-	 * @param sshUsername The <code>String</code> SSH
-	 * user name used login to the SSH host.
 	 * @param localPort The <code>int</code> forwarding
 	 * port number to use on the local machine.
 	 * @throws JSchException If SSH tunnel setup failed.
 	 */
-	SQLSSHSource(final String key, final String host, final int port, final String dbName,
-			final String dbUsername, final String dbPassword, final File sshKey,
-			final String sshUsername, final int localPort) throws JSchException {
+	SQLSSHSource(final String key, final String host, final int sshPort, final String sshUsername,
+			final File sshKey, final int dbPort, final String dbName, final String dbUsername,
+			final String dbPassword, final int localPort) throws JSchException {
 		super(key, "localhost", localPort, dbName, dbUsername, dbPassword);
 		// Setup SSH tunnel.
 		final JSch jsch = new JSch();
 		jsch.addIdentity(sshKey.getAbsolutePath());
-		this.session = jsch.getSession(sshUsername, host);
+		this.session = jsch.getSession(sshUsername, host, sshPort);
 		this.session.setTimeout(0);
 		this.session.setConfig("StrictHostKeyChecking", "no");
 		this.session.connect();
-		this.session.setPortForwardingL(localPort, "127.0.0.1", port);
+		this.session.setPortForwardingL(localPort, "127.0.0.1", dbPort);
 	}
 	
 	/**
@@ -62,34 +64,36 @@ public class SQLSSHSource extends SQLSource {
 	 * identify the attached data source.
 	 * @param host The <code>String</code> address of
 	 * the host.
-	 * @param port The <code>int</code> port of the
-	 * host to connect to.
+	 * @param sshPort The <code>int</code> port used
+	 * to establish the SSH connection.
+	 * @param sshUsername The <code>String</code> SSH
+	 * user name used login to the SSH host.
+	 * @param sshPassword The <code>String</code> SSH
+	 * password used to login to the SSH host.
+	 * @param dbPort The <code>int</code> port of the
+	 * host database to connect to.
 	 * @param dbName The <code>String</code> name of
 	 * the database.
 	 * @param dbUsername The <code>String</code> user
 	 * name used to login to the database.
 	 * @param dbPassword The <code>String</code> password
 	 * used to login to the database.
-	 * @param sshUsername The <code>String</code> SSH
-	 * user name used login to the SSH host.
-	 * @param sshPassword The <code>String</code> SSH
-	 * password used to login to the SSH host.
 	 * @param localPort The <code>int</code> forwarding
 	 * port number to use on the local machine.
 	 * @throws JSchException If SSH tunnel setup failed.
 	 */
-	SQLSSHSource(final String key, final String host, final int port, final String dbName,
-			final String dbUsername, final String dbPassword, final String sshUsername,
-			final String sshPassword, final int localPort) throws JSchException {
+	SQLSSHSource(final String key, final String host, final int sshPort, final String sshUsername,
+			final String sshPassword, final int dbPort, final String dbName, final String dbUsername,
+			final String dbPassword, final int localPort) throws JSchException {
 		super(key, "localhost", localPort, dbName, dbUsername, dbPassword);
 		// Setup SSH tunnel.
 		final JSch jsch = new JSch();
-		this.session = jsch.getSession(sshUsername, host);
+		this.session = jsch.getSession(sshUsername, host, sshPort);
 		this.session.setPassword(sshPassword);
 		this.session.setTimeout(0);
 		this.session.setConfig("StrictHostKeyChecking", "no");
 		this.session.connect();
-		this.session.setPortForwardingL(localPort, "127.0.0.1", port);
+		this.session.setPortForwardingL(localPort, "127.0.0.1", dbPort);
 	}
 	
 	@Override
